3595fbf2595795b648df53a28ca3a1ec5f42a505
Andy Mention <amention@gmail.com>
2014-11-17 01:27:44 -0700

    Add the two data fixtures from GeoJSON file

--- spec/requests/api/v1/properties_spec.rb
+++ spec/requests/api/v1/properties_spec.rb
@@ -3,12 +3,28 @@ require 'rails_helper'
 describe "Properties API" do
   it 'sends a boolean public vs. private response' do
     #Create a geometry
-    file = File.open('spec/fixtures/co_fixtures.geojson') 
-    binding.pry
-    geometries = RGeo::GeoJSON.decode(file, json_parser: :json)
-    geometries.each do |geometry|
-      Property.new(geometry)
+    # each property needs 
+    #   properties - parse fixture as JSON and extract from hash
+    #   geom - parse fixture as RGeo::GeoJSON, build in factory
+    co_factory = RGeo::Cartesian.factory(:srid => 26913, geometry_type: :multi_polygon)
+    file = File.read('spec/fixtures/co_fixtures.geojson') 
+    
+    json_fixture_properties = JSON.parse(file)
+    property_one = Property.new(json_fixture_properties['features'][0]['properties'])
+
+    geometries = RGeo::GeoJSON.decode(file, json_parser: :json, geo_factory: co_factory, geometry_type: :multi_polygon)
+    co_factory.polygon(geometries[0].geometry)
+
+    2.times do |i|
+      property = Property.new(json_fixture_properties['features'][i]['properties'])
+      property.the_geom = geometries[i].geometry
+      property.save
     end
+
+    binding.pry
+#    geometries.each do |geometry|
+#      Property.new(geometry)
+#    end
     
     # test coordinates in west half of state
     # test coordinates in east half of state
