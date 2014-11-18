5dd48bfe317420cbd44a7341546e7a4b25adc6f6
Andy Mention <amention@gmail.com>
2014-11-18 11:11:03 -0700

    shows point information, but still can't find containing property.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,15 @@
 class Property < ActiveRecord::Base
+  def find_property(lat_lng)
+    lat = lat_lng[0]
+    lng = lat_lng[1]
+
+    pt_factory = RGeo::Cartesian.factory(:srid => 26913)
+    user_location = pt_factory.point(lat, lng)
+    
+   parcel =  Property.all.select do |property| 
+      property.wkb_geometry.contains?(user_location)
+    end
+
+   @property = parcel.first
+  end
 end
